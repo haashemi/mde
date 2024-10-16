b3f354b76043c3dec9be4ed89faa65bdd9a6a5f4
Ali Hashemi <mr.ali.haashemi@gmail.com>
2024-10-16 19:06:06 +0330

    fix: fix not defining the fonts' subsets

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,8 @@ import { ContentProvider } from "./_contexts/content-context";
 import { DirectionProvider } from "./_contexts/direction-context";
 import { EditorViewProvider } from "./_contexts/editor-view-context";
 
-const inter = Inter({ variable: "--font-inter" });
-const vazirmatn = Vazirmatn({ variable: "--font-vazirmatn" });
+const inter = Inter({ subsets: ["latin", "latin-ext"], variable: "--font-inter" });
+const vazirmatn = Vazirmatn({ subsets: ["arabic"], variable: "--font-vazirmatn" });
 
 export const metadata: Metadata = {
   title: "Minimal Markdown Editor",
